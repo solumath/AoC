96699d033a3be9e55d76fc80d772c5ceea995136
Daniel <solumath99@gmail.com>
2020-12-01 21:53:18 +0100

    Day 1

---
+++ 1/1.py
@@ -0,0 +1,10 @@
+file = open('file.txt', 'r')
+array = file.read().splitlines()
+array = list(map(int, array))
+
+lenght = len(array)
+for i in range (0, lenght-2):
+    for j in range(i+1, lenght-1):
+        for k in array[j+1:]:
+            if (array[i]+array[j]+k) == 2020:
+                print(array[i]*array[j]*k)
